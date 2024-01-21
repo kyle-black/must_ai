fad5d95369b7e74ec39266b1743d5fc7874d8c40
kyle-black <kpblack87@gmail.com>
2024-01-21 16:20:54 -0600

    updated DB Models

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'must_app.apps.must_aiConfig',
+    'must_app.apps.MustAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
